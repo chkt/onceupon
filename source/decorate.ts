45b0f59306387784e3990ac8707e3f35651ccc6f
chkt <ck@chkt.one>
2019-09-29 13:01:19 +0200

    Add createStreamHandler(), createOutErrHandler() Move log_level string alignment into tokensToString()

--- source/decorate.ts
+++ source/decorate.ts
@@ -9,7 +9,7 @@ export type decorateTokens = (tokens:LogTokens, context:LogContext) => LogTokens
 export function decorateTimeLevelLog(tokens:LogTokens, context:LogContext) : LogTokens {
 	return [
 		createToken(token_type.time, context.time),
-		createToken(token_type.level, getNameOfLevel(context.level).padEnd(6, ' ')),
+		createToken(token_type.level, getNameOfLevel(context.level)),
 		...tokens
 	];
 }
--- source/handler.ts
+++ source/handler.ts
@@ -1,24 +1,28 @@
+import WritableStream = NodeJS.WritableStream;
+
 import { log_level } from "./level";
 import { LogContext } from "./context";
-import { LogTokens } from "./token";
+import { LogTokens, token_type } from "./token";
 
 
 export type handleLog = (tokens:LogTokens, context:LogContext) => Promise<void>;
+
+type Streams = { [P in log_level] : WritableStream };
 
 
 function tokensToString(tokens:LogTokens) : string {
 	let message = '';
 
-	for (const token of tokens) message += `${ token.content } `;
+	for (const token of tokens) {
+		if (token.type === token_type.level) message += token.content.padEnd(8, ' ');
+		else message += `${token.content} `;
+	}
 
 	return message.slice(0, message.length - 1);
 }
 
-function sendMsg(fn:(msg:string) => any, msg:string) : Promise<void> {
-	const ret = fn(msg);
-
-	if (ret instanceof Promise) return ret.then(resolve => resolve());
-	else return Promise.resolve();
+function sendMsgToConsole(fn:(msg:string) => any, msg:string) : Promise<void> {
+	return Promise.resolve(fn(msg));
 }
 
 
@@ -27,29 +31,39 @@ export function consoleHandler(tokens:LogTokens, context:LogContext) : Promise<v
 
 	switch (context.level) {
 		case log_level.fatal:
-		case log_level.error : return sendMsg(console.error, msg);
-		case log_level.warn : return sendMsg(console.warn, msg);
+		case log_level.error : return sendMsgToConsole(console.error, msg);
+		case log_level.warn : return sendMsgToConsole(console.warn, msg);
 		case log_level.notice :
 		case log_level.info :
-		case log_level.verbose : return sendMsg(console.log, msg);
-		case log_level.debug : return sendMsg(console.debug, msg);
+		case log_level.verbose : return sendMsgToConsole(console.log, msg);
+		case log_level.debug : return sendMsgToConsole(console.debug, msg);
 	}
 
 	return Promise.reject();
 }
 
-export function stdHandler(tokens:LogTokens, context:LogContext) : Promise<void> {
-	const msg = tokensToString(tokens) + '\n';
+export function createStreamHandler(streams:Streams) : handleLog {
+	return (tokens, context) => {
+		const msg = tokensToString(tokens);
+		const stream = streams[context.level];
 
-	switch (context.level) {
-		case log_level.fatal :
-		case log_level.error :
-		case log_level.warn : return sendMsg(process.stderr.write, msg);
-		case log_level.notice :
-		case log_level.info :
-		case log_level.verbose :
-		case log_level.debug : return sendMsg(process.stdout.write, msg);
-	}
+		return new Promise((resolve, reject) => {
+			stream.write(msg, err => err === undefined ? resolve() : reject());
+		});
+	};
+}
 
-	return Promise.reject();
+export function createOutErrHandler(
+	out:WritableStream = process.stdout,
+	err:WritableStream = process.stderr
+) : handleLog {
+	return createStreamHandler({
+		[ log_level.fatal ] : err,
+		[ log_level.error ] : err,
+		[ log_level.warn ] : err,
+		[ log_level.notice ] : out,
+		[ log_level.info ] : out,
+		[ log_level.verbose ] : out,
+		[ log_level.debug ] : out
+	});
 }
