67bef9119876d17f42ea5a8545dd1de8e10ebad1
chkt <ck@chkt.one>
2019-10-01 23:49:37 +0200

    Add tags.ts

---
+++ source/tags.ts
@@ -0,0 +1,11 @@
+export function extendTags(tags:string[], add:string) : string[] {
+	if (add.search(/^[ _0-9a-z]+$/) !== 0) return tags;
+
+	const res = [ ...tags, ...add.split(' ')];
+
+	for (let i = res.length - 1, l = tags.length - 1; i > l; i -= 1) {
+		if (res[i] === '' || res.indexOf(res[i], 0) !== i) res.splice(i , 1);
+	}
+
+	return res;
+}
