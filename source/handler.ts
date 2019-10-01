932084ec4d30e9963ecd9d2fda1cf88982384d9e
chkt <ck@chkt.one>
2019-10-01 23:51:34 +0200

    Add tag formatting to tokensToString()

--- source/handler.ts
+++ source/handler.ts
@@ -11,14 +11,18 @@ type Streams = { [P in log_level] : WritableStream };
 
 
 function tokensToString(tokens:LogTokens) : string {
+	let prevType = null;
 	let message = '';
 
 	for (const token of tokens) {
 		if (token.type === token_type.level) message += token.content.padEnd(8, ' ');
+		else if (token.type === token_type.tag) message += (prevType !== token_type.tag ? '.' : '') + `:${ token.content }`;
 		else message += `${token.content} `;
+
+		prevType = token.type;
 	}
 
-	return message.slice(0, message.length - 1);
+	return prevType !== token_type.tag ? message.slice(0, message.length - 1) : message;
 }
 
 function sendMsgToConsole(fn:(msg:string) => any, msg:string) : Promise<void> {
