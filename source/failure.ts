793689620b569a04bb89c81e1662cc7555af054a
chkt <ck@chkt.one>
2019-10-11 17:18:40 +0200

    Add failure.ts

---
+++ source/failure.ts
@@ -0,0 +1,16 @@
+import { LogToken, LogTokens, token_type } from "./token";
+
+
+export const enum failure_type {
+	no_parser = 'NOPARSE'
+}
+
+
+export function createFailureToken(message:string) : LogToken {
+	return { type : token_type.logerr, content : `<${ message }>` };
+}
+
+
+export function parseFail(type:string) : LogTokens {
+	return [ createFailureToken(`${ failure_type.no_parser } ${ type }`)];
+}
