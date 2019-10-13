0d8816dfcf97e660d3d6d549654c9b5243dea6a2
chkt <ck@chkt.one>
2019-10-13 10:45:15 +0200

    Add common.ts

---
+++ source/parser/common.ts
@@ -0,0 +1,15 @@
+import { LogToken, LogTokens, token_type } from "../token";
+
+
+interface ScopeToken extends LogToken {
+	readonly children : LogTokens;
+}
+
+
+export function isScopeToken(token:LogToken) : token is ScopeToken {
+	return 'children' in token;
+}
+
+export function createScopeToken(type:token_type, children:LogTokens) : ScopeToken {
+	return { type, content : '', children };
+}
