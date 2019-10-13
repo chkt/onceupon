ddcf495851e33f0a34a24a73e9c5ba03bf104b5b
chkt <ck@chkt.one>
2019-10-13 10:51:03 +0200

    Add getDelim(), delimitString()

--- source/format.ts
+++ source/format.ts
@@ -1,17 +1,43 @@
 import { LogTokens, token_type } from "./token";
+import { isScopeToken } from "./parser/common";
 
 
-export function tokensToString(tokens:LogTokens) : string {
+function getDelim(prev:token_type|null, next:token_type, depth:number) : string {
+	if (prev === null) return depth === 0 ? '' : '\n'.padEnd(1 + depth, '\t');
+	else if (prev === token_type.level) return ' ';
+	else if (next === token_type.tag) return prev === token_type.tag ? ':' : ' .:';
+	else if (prev === token_type.message_fragment || next === token_type.message_fragment) return '';
+	else if (prev === token_type.property_name) return ' : ';
+	else if (next === token_type.property_name) return ',\n'.padEnd(2 + depth, '\t');
+
+	return ' ';
+}
+
+function delimitString(s:string) : string {
+	if (s.indexOf('\'') === -1) return `'${ s }'`;
+	else if (s.indexOf('"') === -1) return `"${ s }"`;
+	else return `'${ s.replace(/\\*'/g, match => match.length % 2 === 0 ? match : match.slice(0, match.length - 1) + '\\\'') }'`;
+}
+
+
+export function tokensToString(tokens:LogTokens, depth:number = 0) : string {
 	let prevType = null;
 	let message = '';
 
 	for (const token of tokens) {
-		if (token.type === token_type.level) message += token.content.padEnd(8, ' ');
-		else if (token.type === token_type.tag) message += (prevType !== token_type.tag ? '.' : '') + `:${ token.content }`;
-		else message += `${token.content} `;
+		const nextType = token.type;
 
-		prevType = token.type;
+		message += getDelim(prevType, nextType, depth);
+
+		if (nextType === token_type.level) message += token.content.padEnd(7, ' ');
+		else if (nextType === token_type.scalar_string) message += delimitString(token.content);
+		else if (isScopeToken(token)) {
+			if (nextType === token_type.object) message += `{${ tokensToString(token.children, depth + 1) }${ '\n'.padEnd(1 + depth, '\t') }}`;
+		}
+		else message += token.content;
+
+		prevType = nextType;
 	}
 
-	return prevType !== token_type.tag ? message.slice(0, message.length - 1) : message;
+	return message;
 }
