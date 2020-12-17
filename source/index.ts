bf8fb7ed08a7945caa918e6d2e6930b374f4a3a6
chkt <ck@chkt.one>
2020-12-17 10:58:10 +0100

    Change to expose token api

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,5 @@
 export { log_level } from './level';
 export { compose } from './compose';
+export * as token from './token';
 export { LoggerConfig } from './config';
 export { createLogger, Logger } from './logger';
