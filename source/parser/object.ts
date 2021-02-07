c3c1f6d791524a210a4541faba447c001cb0a6d7
chkt <ck@chkt.one>
2021-02-07 16:47:08 +0100

    Add depth test, prototype resolution

--- source/parser/object.ts
+++ source/parser/object.ts
@@ -1,6 +1,13 @@
+import Dict = NodeJS.Dict;
 import { createToken, LogTokens, token_type } from '../token';
 import { ParseContext } from '../parse';
 import { createScopeToken } from './common';
+
+
+interface ObjectProperty {
+	readonly value : unknown;
+	readonly depth : number;
+}
 
 
 export function parseNull() : LogTokens {
@@ -8,6 +15,13 @@ export function parseNull() : LogTokens {
 }
 
 export function parseObject(obj:object, context:ParseContext) : LogTokens {
+	if (context.depthLeft <= 0) {
+		return [ createScopeToken(
+			token_type.object,
+			[ createToken(token_type.object_unresolved, '') ]
+		) ];
+	}
+
 	const refIndex = context.references.indexOf(obj);
 
 	if (refIndex !== -1) {
@@ -17,23 +31,55 @@ export function parseObject(obj:object, context:ParseContext) : LogTokens {
 		) ];
 	}
 
+	const entries:Dict<ObjectProperty> = {};
+	let resolved = false;
+	let source:object|null = obj;
+
+	for (let i = 0; i < context.depthLeft; i += 1) {
+		for (const [ name, value ] of Object.entries(source)) {
+			if (!(name in entries)) entries[name] = { value, depth : i };
+		}
+
+		source = Object.getPrototypeOf(source);
+
+		if (source === Object.prototype || source === null) {
+			resolved = true;
+
+			break;
+		}
+	}
+
+	let items:LogTokens = [];
 	const childContext = {
 		...context,
+		depthLeft : context.depthLeft - 1,
 		references : [ ...context.references, obj ]
 	};
-	const items = [];
 
-	for (const [name, prop] of Object.entries(obj)) {
-		items.push(
+	for (const [ name, prop ] of Object.entries(entries)) {
+		if (prop === undefined) continue;
+
+		items = [
+			...items,
+			...(prop.depth > 0 ? [ createToken(token_type.property_inherited, prop.depth.toFixed()) ] : []),
 			createToken(token_type.property_name, name),
-			...childContext.inferAndParse(prop)
-		);
+			...childContext.inferAndParse(prop?.value)
+		];
 	}
+
+	if (!resolved) items = [ ...items, createToken(token_type.object_unresolved, '') ];
 
 	return [ createScopeToken(token_type.object, items) ];
 }
 
 export function parseArray(arr:unknown[], context:ParseContext) : LogTokens {
+	if (context.depthLeft <= 0) {
+		return [ createScopeToken(
+			token_type.object_array,
+			[ createToken(token_type.object_unresolved, '') ]
+		) ];
+	}
+
 	const refIndex = context.references.indexOf(arr);
 
 	if (refIndex !== -1) {
@@ -45,6 +91,7 @@ export function parseArray(arr:unknown[], context:ParseContext) : LogTokens {
 
 	const childContext = {
 		...context,
+		depthLeft: context.depthLeft - 1,
 		references : [ ...context.references, arr ]
 	};
 	const items = [];
