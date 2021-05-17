40782ee4dfb0a003b7f46274885ad623a4840246
chkt <ck@chkt.one>
2021-05-17 17:28:55 +0200

    Add Log to module exports

--- source/index.ts
+++ source/index.ts
@@ -2,4 +2,5 @@ export { log_level } from './level';
 export { compose } from './compose';
 export * as token from './token';
 export { LoggerConfig } from './config';
+export { Log } from './context';
 export { createLogger, Logger } from './logger';
