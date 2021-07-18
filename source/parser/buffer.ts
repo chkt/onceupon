86e22e494ab739881d709fa21a174440da089f2b
chkt <ck@chkt.one>
2021-07-18 13:16:10 +0200

    Change buffer encoding in parseBytes()

--- source/parser/buffer.ts
+++ source/parser/buffer.ts
@@ -1,17 +1,24 @@
-import { createToken, LogToken, LogTokens, token_type } from '../token';
-import { createScopeToken } from './common';
+import { createToken, LogTokens, token_type } from '../token';
+import { ParseContext } from '../parse';
 
 
-export function parseBytes(bytes:Uint8Array) : LogTokens {
+export function parseBytes(bytes:Uint8Array, context:ParseContext) : LogTokens {
 	const type = Object.getPrototypeOf(bytes).constructor.name;
-	const tokens:LogToken[] = [];
 
-	bytes.forEach(byte => {
-		tokens.push(createToken(token_type.scalar_byte, byte.toString(16).padStart(2, '0')));
-	});
+	let hex = `${ bytes.byteLength.toString(16) }+0-` + bytes
+		.slice(0, context.bytesHead)
+		.reduce((prev, value) => prev + value.toString(16).padStart(2, '0'), '');
+
+	if (bytes.byteLength > context.bytesHead && context.bytesTail !== 0) {
+		const offset = Math.max(context.bytesHead, bytes.byteLength - context.bytesTail);
+
+		hex += `+${ offset.toString(16) }-` + bytes
+			.slice(offset)
+			.reduce((prev, value) => prev + value.toString(16).padStart(2, '0'), '');
+	}
 
 	return [
 		createToken(token_type.type_name, type),
-		createScopeToken(token_type.object_bytes, tokens)
+		createToken(token_type.object_bytes, hex)
 	];
 }
